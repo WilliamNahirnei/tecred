078fc22e5306597e40a4951b202d8dfd7a4f0f22
unknown <william.nahirnei@hotmail.com>
2022-03-30 19:07:23 -0300

    Create Category Migration

--- database/migrations/2022_03_30_215351_create_table_category.php
+++ database/migrations/2022_03_30_215351_create_table_category.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreatePasswordResetsTable extends Migration
+class CreateTableCategory extends Migration
 {
     /**
      * Run the migrations.
@@ -13,10 +13,11 @@ class CreatePasswordResetsTable extends Migration
      */
     public function up()
     {
-        Schema::create('password_resets', function (Blueprint $table) {
-            $table->string('email')->index();
-            $table->string('token');
-            $table->timestamp('created_at')->nullable();
+        Schema::create('category', function (Blueprint $table) {
+            $table->increments('idCategory')->unique();
+            $table->string('nameCategory', 255);
+            $table->addColumn('tinyInteger', 'statusCategory', ['length' => 1]);
+            $table->timestamps();
         });
     }
 
@@ -27,6 +28,6 @@ class CreatePasswordResetsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('password_resets');
+        Schema::dropIfExists('category');
     }
 }
