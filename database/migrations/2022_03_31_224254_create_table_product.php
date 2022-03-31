7f5c48154a1b4f3537ff9aa369b483e24a9c042a
unknown <william.nahirnei@hotmail.com>
2022-03-31 20:02:23 -0300

    Crete migration product

---
+++ database/migrations/2022_03_31_224254_create_table_product.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTableProduct extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('table_product', function (Blueprint $table) {
+            $table->increments('idProduct')->unique();
+            $table->string('nameProduct', 255);
+            $table->integer('quantityProduct');
+            $table->addColumn('tinyInteger', 'statusProduct', ['length' => 1]);
+            $table->foreign('idCategory')->references('idCategory')->on('category');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('table_product');
+    }
+}
