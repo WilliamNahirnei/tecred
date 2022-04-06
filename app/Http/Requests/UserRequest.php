2cad626da3a6fcfdbfda80c3f2c2c3696ef9bc3e
unknown <william.nahirnei@hotmail.com>
2022-04-06 19:09:07 -0300

    Create autenticate jwt structure

---
+++ app/Http/Requests/UserRequest.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Http\Requests\CustomRulesRequest;
+
+class UserRequest extends CustomRulesRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return Bool
+     */
+    public function authorize(): Bool
+    {
+        return true;
+    }
+
+    /**
+     * @return Array
+     */
+    public function validateDefault(): Array
+    {
+        return [
+            // Your default validation
+        ];
+    }
+
+    /**
+     * @return Array
+     */
+    public function validateToStore(): Array
+    {
+        return [
+            'nameUser' => 'required|min:4|max:50',
+            'email' => 'required|email|min:4|max:60',
+            'password' => 'required|min:5|max:255',
+        ];
+
+    }
+
+    /**
+     * @return Array
+     */
+    public function validateToUpdate(): Array
+    {
+        return [
+            // 'name' => 'max:60',
+        ];
+    }
+
+    /**
+     * @return Array
+     */
+    public function validateToDestroy(): Array
+    {
+        return [
+            // 'id' => 'required',
+        ];
+    }
+
+    /**
+     * @return Array
+     */
+    public function messages(): Array
+    {
+        return [
+            // 'id.required' => 'O id é obrigatório!',
+        ];
+    }
+}
