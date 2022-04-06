2e17778e9e485857ce1a75e4ddddcadd5c6ae7a5
unknown <william.nahirnei@hotmail.com>
2022-04-06 18:53:06 -0300

    Create dependenci for jwt and initial config, create migration for table user, and create user model

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,43 +2,44 @@
 
 namespace App\Models;
 
-use Illuminate\Contracts\Auth\MustVerifyEmail;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
-use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Database\Eloquent\Model;
 
-class User extends Authenticatable
+class User extends Model
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    use Traits\Scope;
+
+    protected $guarded = ['idUser'];
+    protected $table = 'user';
+    protected $primaryKey = 'idUser';
 
     /**
-     * The attributes that are mass assignable.
+     * Attributes that are mass assignable.
      *
-     * @var array<int, string>
+     * @var array
      */
     protected $fillable = [
-        'name',
-        'email',
-        'password',
+        "nameUser",
+        "email",
+        "password"
     ];
 
-    /**
-     * The attributes that should be hidden for serialization.
+        /**
+     * Get the identifier that will be stored in the subject claim of the JWT.
      *
-     * @var array<int, string>
+     * @return mixed
      */
-    protected $hidden = [
-        'password',
-        'remember_token',
-    ];
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
 
     /**
-     * The attributes that should be cast.
+     * Return a key value array, containing any custom claims to be added to the JWT.
      *
-     * @var array<string, string>
+     * @return array
      */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-    ];
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
 }
